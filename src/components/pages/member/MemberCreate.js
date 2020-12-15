1c227fffba81cb31fa3d23da35228cc8da6cf2ff
ali masoomzadeh <eng.masoomzadeh@gmail.com>
2020-12-15 20:30:54 +0330

    pages create

---
+++ src/components/pages/member/MemberCreate.js
@@ -0,0 +1,11 @@
+
+export default function MemberCreate() {
+
+
+
+    return (
+        <div>Create</div>
+    );
+}
+
+
---
+++ src/components/pages/member/MemberMaster.js
@@ -0,0 +1,11 @@
+
+export default function MemberMaster() {
+
+
+
+    return (
+        <div>Master</div>
+    );
+}
+
+
