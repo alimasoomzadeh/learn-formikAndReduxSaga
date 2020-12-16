32c69fd70594ed5d7a95ec1fd0e7ad2317974bba
ali masoomzadeh <eng.masoomzadeh@gmail.com>
2020-12-16 08:47:49 +0330

    add formik lib add style component lib

--- src/components/pages/member/MemberCreate.js
+++ src/components/pages/member/MemberCreate.js
@@ -1,11 +1,99 @@
+import React from 'react';
+import {useFormik} from 'formik';
+import styled from 'styled-components';
+
+
+const From = styled.form`
+  background: red;
+text-align: center;
+`;
+
+const validate = values => {
+    const errors = {};
+    if (!values.firstName) {
+        errors.firstName = 'Required';
+    } else if (values.firstName.length > 15) {
+        errors.firstName = 'Must be 15 characters or less';
+    }
+
+    if (!values.lastName) {
+        errors.lastName = 'Required';
+    } else if (values.lastName.length > 20) {
+        errors.lastName = 'Must be 20 characters or less';
+    }
+
+    if (!values.email) {
+        errors.email = 'Required';
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+        errors.email = 'Invalid email address';
+    }
+
+    return errors;
+};
 
 export default function MemberCreate() {
-
-
-
+    const formik = useFormik({
+        initialValues: {
+            firstName: '',
+            lastName: '',
+            email: '',
+        },
+        validate,
+        onSubmit: values => {
+            alert(JSON.stringify(values, null, 2));
+        },
+    });
     return (
-        <div>Create</div>
+            <From onSubmit={formik.handleSubmit}>
+                <div>
+                    <label htmlFor="firstName">First Name</label>
+                    <input
+                        id="firstName"
+                        name="firstName"
+                        type="text"
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        value={formik.values.firstName}
+                    />
+                </div>
+                <div>
+                    {formik.touched.firstName && formik.errors.firstName ? (
+                        <div>{formik.errors.firstName}</div>
+                    ) : null}
+                </div>
+                <div>
+                    <label htmlFor="lastName">Last Name</label>
+                    <input
+                        id="lastName"
+                        name="lastName"
+                        type="text"
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        value={formik.values.lastName}
+                    />
+                </div>
+                <div>
+                    {formik.touched.lastName && formik.errors.lastName ? (
+                        <div>{formik.errors.lastName}</div>
+                    ) : null}
+                </div>
+                <div>
+                    <label htmlFor="email">Email Address</label>
+                    <input
+                        id="email"
+                        name="email"
+                        type="email"
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        value={formik.values.email}
+                    />
+                </div>
+                <div>
+                    {formik.touched.email && formik.errors.email ? (
+                        <div>{formik.errors.email}</div>
+                    ) : null}
+                </div>
+                <button type="submit">Submit</button>
+            </From>
     );
-}
-
-
+};
